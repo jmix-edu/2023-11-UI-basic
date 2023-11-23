1365b60df9bbc82b01efe559836950f85ceca3c6
vashenkoda <d.vaschenko@haulmont.net>
2023-11-23 17:21:36 +0300

    Finished

--- src/main/java/com/company/jmixpm/screen/project/ProjectBrowse.java
+++ src/main/java/com/company/jmixpm/screen/project/ProjectBrowse.java
@@ -1,10 +1,36 @@
 package com.company.jmixpm.screen.project;
 
+import com.company.jmixpm.screen.projecttasksbrowser.ProjectTasksBrowser;
+import io.jmix.ui.ScreenBuilders;
+import io.jmix.ui.action.Action;
+import io.jmix.ui.component.GroupTable;
 import io.jmix.ui.screen.*;
 import com.company.jmixpm.entity.Project;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @UiController("Project.browse")
 @UiDescriptor("project-browse.xml")
 @LookupComponent("projectsTable")
 public class ProjectBrowse extends StandardLookup<Project> {
+
+    @Autowired
+    private GroupTable<Project> projectsTable;
+    @Autowired
+    private ScreenBuilders screenBuilders;
+
+    @Subscribe("projectsTable.showTasks")
+    public void onProjectsTableShowTasks(final Action.ActionPerformedEvent event) {
+
+        Project selected = projectsTable.getSingleSelected();
+        if (selected == null) {
+            return;
+        }
+
+        screenBuilders.screen(this)
+                .withScreenClass(ProjectTasksBrowser.class)
+                .build()
+                .withProject(selected)
+                .show();
+        
+    }
 }
